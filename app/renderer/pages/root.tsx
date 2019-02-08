06dedcb17ad8a50271c0b68b48ffcf8ea6b724ba
Dawid Gorczyca <dawid.gorczyca@globallogic.com>
2019-02-08 13:36:19 +0100

    finished ipc example

--- app/renderer/pages/root.tsx
+++ app/renderer/pages/root.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 
-export default () => (
-  <div style={{ textAlign: 'center', marginTop: 100 }}>
-    <h3><Link to='/start' id='start'>Start</Link></h3>
-  </div>
-)
+const {remote, ipcRenderer} = window.require('electron')
+
+class Root extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      status: ''
+    }
+  }
+  public componentDidMount() {
+    ipcRenderer.on('ping', (event, arg) => {
+      // tslint:disable-next-line:no-console
+      this.setState({status: arg.counter})
+    })
+  }
+  public render() {
+    return(
+      <div style={{ textAlign: 'center', marginTop: 100 }}>
+        <h3><Link to='/start' id='start'>Start</Link></h3>
+        <p>{this.state.status}</p>
+      </div>
+    )
+  }
+}
+
+export default Root
